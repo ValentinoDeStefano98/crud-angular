c405b44acb5f5a936da8dbf149ce4f07b351bc69
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-09-29 15:31:38 +0200

    First commit

---
+++ src/app/services/tutorial.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8080/api/tutorials';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any>{
+    return this.http.get(baseUrl);
+  }
+
+  get(id: any): Observable<any>{
+    /* Using template literals to concatenate the baseUrl with the id. */
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  /**
+   * It takes in a data object, and returns an observable of type any
+   * @param data - The data to be sent to the server.
+   * @returns Observable<any>
+   */
+  create(data: any): Observable<any>{
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any>{
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(title: any): Observable<any> {
+    return this.http.get(`${baseUrl}?title=${title}`);
+  }
+}
